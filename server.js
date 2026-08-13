498740c0447ecc5764b54aec1b5c005a71d55d95
Godugu Keerthana <godugukeerthana10@gmail.com>
2026-08-13 15:34:43 +0530

    addedfrontend

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ let express = require('express');
 
 let app = express();
 
-let port = 3000;
+let port = process.env.PORT || 8080;
+
+app.use(express.static("frontend"))
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
