1e88b3664f087d2e343b0dda47537f5ef518771b
Godugu Keerthana <godugukeerthana10@gmail.com>
2026-08-14 11:09:56 +0530

    randomuser

--- frontend/index.js
+++ frontend/index.js
@@ -28,4 +28,17 @@ function toggle(){
  document.getElementById("card-gender").innerText =users[curIndex].gender
  //gender needs to be updated
 }
-
+function random(){
+  //url = "https://randomuser.me/api/"
+  fetch("https://randomuser.me/api/")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    var detail=data.results[0];
+    var name = detail.name.title + " " + detail.name.first + " " + detail.name.last;
+    document.getElementById("card-img").src = detail.picture.large;
+    document.getElementById("card-name").innerText = name;
+    document.getElementById("card-gender").innerText = detail.gender;
+  });
+}
